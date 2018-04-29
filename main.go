8ac98b3a00863925c1506b2269f1efbffabc3657
Tauki Tahmid <ttauki@gmail.com>
2018-04-29 14:41:49 +0600

    Refactor and check flags or take input

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/urfave/cli"
 	"fmt"
-	"time"
-	"os"
-	"errors"
-	"github.com/cb-migrate/models"
 	"github.com/cb-migrate/connection"
+	"github.com/cb-migrate/models"
+	"github.com/urfave/cli"
+	"os"
+	"time"
 )
 
 func main() {
@@ -18,46 +17,58 @@ func main() {
 	app.Usage = "Lorem ipsum"
 	app.Version = fmt.Sprintf("%s-alpha", time.Now().Local().Format("00.00.00"))
 
-	app.Flags = []cli.Flag {
+	app.Flags = []cli.Flag{
 
 		// source
-		cli.StringFlag {
-			Name: "source",
-			Value: "localhost:8091",
-			Usage: "The source from where to get the buckets",
-			Destination: &source.Address,
+		cli.StringFlag{
+			Name:        "source",
+			Value:       "localhost",
+			Usage:       "The source from where to get the buckets",
+			Destination: &source.DBHost,
 		},
-		cli.StringFlag {
-			Name: "source_user",
-			Value: "Administrator",
-			Usage: "Source Cluster Username",
-			Destination: &source.Username,
+		cli.StringFlag{
+			Name:        "source_user",
+			Value:       "Administrator",
+			Usage:       "Source Cluster Username",
+			Destination: &source.DBUser,
 		},
-		cli.StringFlag {
-			Name: "source_pass",
-			Value: "password",
-			Usage: "Source Cluster Password",
-			Destination: &source.Password,
+		cli.StringFlag{
+			Name:        "source_pass",
+			Value:       "password",
+			Usage:       "Source Cluster Password",
+			Destination: &source.DBPassword,
+		},
+		cli.StringFlag{
+			Name:        "source_port",
+			Value:       "8091",
+			Usage:       "Port the source is running",
+			Destination: &source.DBPort,
 		},
 
 		// target
 		cli.StringFlag{
-			Name: "target",
-			Value: "localhost:8091",
-			Usage: "The location where to post the buckets from source",
-			Destination: &target.Address,
+			Name:        "target",
+			Value:       "localhost",
+			Usage:       "The location where to post the buckets from source",
+			Destination: &target.DBHost,
 		},
-		cli.StringFlag {
-			Name: "target_user",
-			Value: "Administrator",
-			Usage: "Destination Cluster Username",
-			Destination: &target.Username,
+		cli.StringFlag{
+			Name:        "target_user",
+			Value:       "Administrator",
+			Usage:       "Destination Cluster Username",
+			Destination: &target.DBUser,
 		},
-		cli.StringFlag {
-			Name: "target_pass",
-			Value: "password",
-			Usage: "Destination Cluster Password",
-			Destination: &target.Password,
+		cli.StringFlag{
+			Name:        "target_pass",
+			Value:       "password",
+			Usage:       "Destination Cluster Password",
+			Destination: &target.DBPassword,
+		},
+		cli.StringFlag{
+			Name:        "target_port",
+			Value:       "8091",
+			Usage:       "Port the target is running",
+			Destination: &target.DBPort,
 		},
 	}
 
@@ -66,21 +77,29 @@ func main() {
 			Name:  "run",
 			Usage: "Run the cli app",
 			Action: func(c *cli.Context) {
-				err := checkFlags(&source, &target)
-				if err != nil {
-					panic(err.Error())
+
+				for {
+					if checkFlags(&source, "Source") {
+						break
+					}
 				}
 
-				err = checkIfUrl(source.Address, target.Address)
-				if err != nil {
-					panic(err.Error())
+				for {
+					if checkFlags(&target, "target") {
+						break
+					}
 				}
 
-				sCtrl, err := connection.GetBucketServer(&source)
-				if err != nil {} // todo: error handle
-				tCtrl, err := connection.GetBucketServer(&target)
-				if err != nil {} // todo: error handle
+				sCtrl, err := connection.GetClusterServer(&source)
+				if err != nil {
+				} // todo: error handle
 
+				tCtrl, err := connection.GetClusterServer(&target)
+				if err != nil {
+				} // todo: error handle
+
+				fmt.Println(sCtrl)
+				fmt.Println(tCtrl)
 			},
 		},
 	}
@@ -90,14 +109,41 @@ func main() {
 	}
 }
 
-func checkFlags(source, target *models.Cluster) (error) {
-	if source.Address == "" || source.Username == "" || source.Password == "" ||
-		target.Address == "" || target.Username == "" || target.Password == "" {
-			return errors.New("check flags")
+func checkFlags(data *models.Cluster, context string) (bool) {
+	check := true
+
+	if data.DBHost == "" {
+		check = false
+		fmt.Printf("Please input %s address: ", context)
+		fmt.Scanf("%s", data.DBHost)
+		for {
+			if checkIfUrl(data.DBHost) {
+				break
+			}
+		}
 	}
-	return nil
+
+	if data.DBPort == "" {
+		check = false
+		fmt.Printf("Please input %s port: ", context)
+		fmt.Scanf("%s", data.DBPort)
+	}
+
+	if data.DBUser == "" {
+		check = false
+		fmt.Print("Please input DB Username: ")
+		fmt.Scanf("%s", data.DBUser)
+	}
+
+	if data.DBPassword == "" {
+		check = false
+		fmt.Print("Please input DB password: ")
+		fmt.Scanf("%s", data.DBPassword)
+	}
+
+	return check
 }
 
-func checkIfUrl(source, target string) (error) {
-	return nil
+func checkIfUrl(source string) (bool) {
+	return true
 }
