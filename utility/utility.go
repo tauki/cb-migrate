f37d1d8ef14ca4110d3a13f1e32b46e767f2c0b1
Tauki Tahmid <ttauki@gmail.com>
2018-04-30 19:20:39 +0600

    make sure the flags are full

---
+++ utility/utility.go
@@ -0,0 +1,57 @@
+package utility
+
+import (
+	"github.com/cb-migrate/models"
+	"fmt"
+)
+
+func CheckFlags(data *models.Cluster, context string) (bool) {
+	check := true
+
+	if data.DBHost == "" {
+		check = false
+		fmt.Printf("Please input %s address: \n", context)
+		fmt.Scanln(&data.DBHost)
+		for {
+			if checkIfUrl(data.DBHost) {
+				break
+			}
+		}
+	}
+
+	if data.DBPort == "" {
+		check = false
+		fmt.Printf("Please input %s port: \n", context)
+		fmt.Scanln(&data.DBPort)
+	}
+
+	if data.DBUser == "" {
+		check = false
+		fmt.Printf("Please input %s DB Username: ", context)
+		fmt.Scanln(&data.DBUser)
+	}
+
+	if data.DBPassword == "" {
+		check = false
+		fmt.Printf("Please input %s DB password: ", context)
+		fmt.Scanln(&data.DBPassword)
+	}
+
+	return check
+}
+
+func checkIfUrl(source string) (bool) {
+	return true
+}
+
+func GetBucketCreds(name string) (*models.Bucket) {
+	var bucket models.Bucket
+
+	bucket.BucketName = name
+
+	fmt.Printf("Please input the password for %s\n", name)
+	fmt.Println("leave empty for none")
+	fmt.Scanln(&bucket.BucketPassword)
+
+	return &bucket
+}
