7775b10d90b49bca2d876c0ab96fe4d3971e9715
Tauki Tahmid <ttauki@gmail.com>
2018-04-30 19:20:59 +0600

    Remove misc from main to utility

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/cb-migrate/connection/cluster"
 	"github.com/cb-migrate/models"
+	"github.com/cb-migrate/utility"
 	"github.com/urfave/cli"
 	"os"
 	"time"
@@ -22,25 +23,21 @@ func main() {
 		// source
 		cli.StringFlag{
 			Name:        "source",
-			//Value:       "localhost",
 			Usage:       "The source from where to get the buckets",
 			Destination: &source.DBHost,
 		},
 		cli.StringFlag{
 			Name:        "source_user",
-			//Value:       "Administrator",
 			Usage:       "Source Cluster Username",
 			Destination: &source.DBUser,
 		},
 		cli.StringFlag{
 			Name:        "source_pass",
-			//Value:       "password",
 			Usage:       "Source Cluster Password",
 			Destination: &source.DBPassword,
 		},
 		cli.StringFlag{
 			Name:        "source_port",
-			//Value:       "8091",
 			Usage:       "Port the source is running",
 			Destination: &source.DBPort,
 		},
@@ -48,25 +45,21 @@ func main() {
 		// target
 		cli.StringFlag{
 			Name:        "target",
-			//Value:       "localhost",
 			Usage:       "The location where to post the buckets from source",
 			Destination: &target.DBHost,
 		},
 		cli.StringFlag{
 			Name:        "target_user",
-			//Value:       "Administrator",
 			Usage:       "Destination Cluster Username",
 			Destination: &target.DBUser,
 		},
 		cli.StringFlag{
 			Name:        "target_pass",
-			//Value:       "password",
 			Usage:       "Destination Cluster Password",
 			Destination: &target.DBPassword,
 		},
 		cli.StringFlag{
 			Name:        "target_port",
-			//Value:       "8091",
 			Usage:       "Port the target is running",
 			Destination: &target.DBPort,
 		},
@@ -80,13 +73,13 @@ func main() {
 
 				for {
 					fmt.Println(source)
-					if checkFlags(&source, "Source") {
+					if utility.CheckFlags(&source, "Source") {
 						break
 					}
 				}
 
 				for {
-					if checkFlags(&target, "target") {
+					if utility.CheckFlags(&target, "target") {
 						break
 					}
 				}
@@ -112,41 +105,3 @@ func main() {
 	}
 }
 
-func checkFlags(data *models.Cluster, context string) (bool) {
-	check := true
-
-	if data.DBHost == "" {
-		check = false
-		fmt.Printf("Please input %s address: \n", context)
-		fmt.Scanln(&data.DBHost)
-		for {
-			if checkIfUrl(data.DBHost) {
-				break
-			}
-		}
-	}
-
-	if data.DBPort == "" {
-		check = false
-		fmt.Printf("Please input %s port: \n", context)
-		fmt.Scanln(&data.DBPort)
-	}
-
-	if data.DBUser == "" {
-		check = false
-		fmt.Printf("Please input %s DB Username: ", context)
-		fmt.Scanln(&data.DBUser)
-	}
-
-	if data.DBPassword == "" {
-		check = false
-		fmt.Printf("Please input %s DB password: ", context)
-		fmt.Scanln(&data.DBPassword)
-	}
-
-	return check
-}
-
-func checkIfUrl(source string) (bool) {
-	return true
-}
